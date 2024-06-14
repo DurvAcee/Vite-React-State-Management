7fd82ce1d3605ac8c93024691fb6616f922bb31e
Durvesh Danve <Durvesh.Danve@veritas.com>
2024-06-14 12:58:06 +0530

    UUID Impl added

--- src/components/Emojis.jsx
+++ src/components/Emojis.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 export default function Emojis() {
-    const [emoji, setEmoji] = useState(['😊']);
+    const [emoji, setEmoji] = useState([{id: uuid(), emoji:'😊'}]);
     const emojiArray = ['😊', '😢', '😒', '🤣', '🍇'];
     const addEmo = () => {
         let i = Math.floor(Math.random()*emojiArray.length + 1)
         setEmoji((e)=>(
-            [...e, emojiArray[i]]
+            [...e, {id: uuid(), emoji:emojiArray[i]}]
         ));
     }
 
@@ -15,7 +16,7 @@ export default function Emojis() {
             <div>
             <h3> Emojis </h3>
             {emoji.map((e)=>(
-                <span style={{fontSize: '2rem'}}> {e} </span>
+                <span key = {e.id} style={{fontSize: '2rem'}}> {e.emoji} </span>
             ))}
             </div>
             <button onClick = {addEmo}> Add New Emoji </button>
